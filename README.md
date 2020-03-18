7d7c24af8217ab2190ff62d683a23d28ae42fa77
chrisalexander <chris@chris-alexander.co.uk>
2020-03-18 20:27:44 +0000

    Initial blinks implementation

---
+++ README.md
@@ -0,0 +1,29 @@
+# Blinks
+
+Lights
+
+## Message Spec
+
+Format: \<channel> "message"
+
+**Inputs**
+
+* \<blinks.front> on
+  * Turns the front LED on
+* \<blinks.front> off
+  * Turns the front LED off
+* \<blinks.top> on
+  * Turns the top LED on
+* \<blinks.top> off
+  * Turns the top LED off
+
+**Outputs**
+
+* \<blinks.front.status> "front.on"
+  * The front LED has switched on
+* \<blinks.front.status> "front.off"
+  * The front LED has switched off
+* \<blinks.top.status> "top.on"
+  * The top LED has switched on
+* \<blinks.top.status> "top.off"
+  * The top LED has switched off
---
+++ blinks.py
@@ -0,0 +1,62 @@
+import systemd.daemon
+import redis
+
+
+def execute():
+    print('Startup')
+
+    top_output = '6'
+    front_output = '7'
+    top_pfd_channel = 'pfd.output.' + top_output
+    front_pfd_channel = 'pfd.output.' + front_output
+    top_channel_status = top_pfd_channel + '.status'
+    front_channel_status = front_pfd_channel + '.status'
+    top_channel_on = 'output.' + top_output + '.on'
+    top_channel_off = 'output.' + top_output + '.off'
+    front_channel_on = 'output.' + front_output + '.on'
+    front_channel_off = 'output.' + front_output + '.off'
+    top_request_channel = 'blinks.top'
+    front_request_channel = 'blinks.front'
+
+    r = redis.Redis(host='192.168.0.1', port=6379,
+                    db=0, decode_responses=True)
+    p = r.pubsub(ignore_subscribe_messages=True)
+    p.subscribe(top_request_channel, front_request_channel,
+                top_channel_status, front_channel_status)
+
+    r.publish('services', 'blinks.on')
+    systemd.daemon.notify('READY=1')
+    print('Startup complete')
+
+    try:
+        for message in p.listen():
+            if message['channel'] == top_request_channel:
+                if message['data'] == 'on':
+                    r.publish(top_pfd_channel, 'on')
+                else:
+                    r.publish(top_pfd_channel, 'off')
+            if message['channel'] == front_request_channel:
+                if message['data'] == 'on':
+                    r.publish(front_pfd_channel, 'on')
+                else:
+                    r.publish(front_pfd_channel, 'off')
+
+            elif message['channel'] == top_channel_status:
+                if message['data'] == top_channel_on:
+                    r.publish('blinks.front.status', 'front.on')
+                elif message['data'] == top_channel_off:
+                    r.publish('blinks.front.status', 'front.off')
+
+            elif message['channel'] == front_channel_status:
+                if message['data'] == front_channel_on:
+                    r.publish('blinks.top.status', 'top.on')
+                elif message['data'] == front_channel_off:
+                    r.publish('blinks.top.status', 'top.off')
+    except:
+        p.close()
+        r.publish('services', 'blinks.off')
+        print('Goodbye')
+
+
+if __name__ == '__main__':
+    execute()
